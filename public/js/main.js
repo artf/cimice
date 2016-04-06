8d690865d19a90137ceaf7e6cf2282cbc475e350
Artur Arseniev <artur.catch@hotmail.it>
2016-04-06 23:23:30 +0200

    OMG it's working

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  // Recording
   var recorder = new cimice.Recorder({
     target: document.getElementById('to-rec')
   });
@@ -18,6 +19,31 @@ $(document).ready(function(){
 
   recorder.on('recording', function(e){
     console.log('clicked '+ e.type);
+  });
+
+  // Playing
+  var player = new cimice.Player({
+    target: document.getElementById('to-view')
+  });
+
+  var btnPlay = document.getElementById('play');
+  var btnStop = document.getElementById('stop');
+
+  btnStop.addEventListener('click', function(){
+    player.stop();
+  });
+
+  btnPlay.addEventListener('click', function(){
+    player.setMovie(recorder.getMovie());
+    player.play();
+  });
+
+  player.on('play',function(){
+    btnPlay.className += " active";
+  });
+
+  player.on('stop',function(){
+    btnPlay.className = btnPlay.className.replace(/\bactive\b/,'');
   });
 
 })
