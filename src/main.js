0b367fdc72ba60ff8474cd0f1eca101261c1ffd5
Artur Arseniev <artur.catch@hotmail.it>
2016-04-06 13:30:37 +0200

    Fixed Camera export

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Frame from './components/Frame';
-import Camera from './components/Camera';
 import Movie from './components/Movie';
 
-export { Frame, Camera, Movie }
+export { Frame, Movie }
